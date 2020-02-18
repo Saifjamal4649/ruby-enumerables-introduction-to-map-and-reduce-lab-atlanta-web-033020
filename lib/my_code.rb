1dedb39021913f3825f8b3dbe868a8d5d1d45abb
Saif Jamal <saifjamal4649@gmail.com>
2020-02-18 01:36:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,5 +12,11 @@ def map_to_negativize(source_array)
 end 
 
 def map_to_no_change(source_array)
-  
+  new_array = []
+  i = 0 
+  while i < source_array.length do 
+    new_array.push(source_array[i])
+    i += 1 
+  end 
+  return new_array
 end 
