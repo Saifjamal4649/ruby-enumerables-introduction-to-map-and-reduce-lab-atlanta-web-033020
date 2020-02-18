94e887fb9d39aba03e7ba1c7da0ff8799670658a
Saif Jamal <saifjamal4649@gmail.com>
2020-02-18 01:44:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,13 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
-  
+  sum = starting_point
+  i = 0 
+  while i < source_array.length do 
+    sum += (source_array[i])
+    i += 1 
+  end 
+  return sum 
 end 
 
 
