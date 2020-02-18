338db021d1b3d006186166d25901235167498ffd
Saif Jamal <saifjamal4649@gmail.com>
2020-02-18 01:50:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,7 +63,10 @@ end
 def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.length do 
-    return 
+    return true if source_array[i] === true 
+    i += 1 
+  end 
+  return false 
 end 
 
 
